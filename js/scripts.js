7173311f54b70af24f74f4622661ff543e300482
Matt <mattsdni@gmail.com>
2016-03-06 22:44:15 -0800

    add/remove course works

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+function removeClassFromSemester(class_name, semester_name) {
+	//get semester course list
+	var semester_node = document.getElementById(semester_name+"courselist");
+	//console.log(semester_node);
+
+	//remove the right course
+	semester_node.getElementsByClassName("active")[0].remove();
+
+}
+
+//Adds a class to a semester
+function addClassToSemester(class_name, semester_name) {
+	var course_select = 'course' + class_name;
+	var course_node = document.getElementById(course_select);
+
+	//make a copy (so the course isn't removed from the course list)
+	var course = course_node.cloneNode(true);
+	
+	//update id
+	course.id += semester_name;
+
+	var semester_node = document.getElementById(semester_name+"courselist");
+
+	//get button
+	var button = course.getElementsByClassName("collapsible-body")[0].getElementsByClassName("center-btn")[0].getElementsByClassName("btn")[0];
+	
+	//set button to 'remove'
+	button.textContent = "Remove";
+	
+	//set button color to red
+	button.className += " red";
+	
+	//set button functionality
+	button.setAttribute('onclick','removeClassFromSemester("csce144", "fall2016")');
+
+	//add to semester course list
+	semester_node.appendChild(course);
+}
